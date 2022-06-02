aa47ba60480a823e9aa1a2d924eada6cb04f42f1
Gleb Limanskiy <33000609+UnicalN@users.noreply.github.com>
2022-06-02 22:25:41 +0300

    MVP

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import FiltersPresenter from './presenter/filters-presenter.js';
+import PointListPresenter from './presenter/point-list-presenter.js';
+
+const filtersContainer = document.querySelector('.trip-controls__filters');
+const pointListContainer = document.querySelector('.trip-events');
+
+
+const pointListPresenter = new PointListPresenter();
+const filtersPresenter = new FiltersPresenter();
+
+pointListPresenter.init(pointListContainer);
+filtersPresenter.init(filtersContainer);
---
+++ src/presenter/point-list-presenter.js
@@ -0,0 +1,28 @@
+import {render} from '../render.js';
+import EditPointView from '../view/edit-point-view.js';
+import NewPointView from '../view/new-point-view.js';
+import PointListView from '../view/point-list-view.js';
+import PointView from '../view/point-view.js';
+import SortView from '../view/sort-view.js';
+
+
+export default class PointListPresenter {
+  editPointComponent = new EditPointView();
+  pointListComponent = new PointListView();
+
+  init = (pointListContainer) => {
+    this.pointListContainer = pointListContainer;
+    render(new SortView(), this.pointListContainer);
+    render(this.pointListComponent, this.pointListContainer); // this. вместо new тк объявлено ранее для повторяющихся элементов
+    render(new EditPointView(), this.pointListComponent.getElement());
+    render(new NewPointView(), this.pointListComponent.getElement());
+
+    //render(, this.pointListComponent.getElement());
+
+    //render;
+
+    for (let i = 0; i < 3; i++) {
+      render(new PointView(), this.pointListComponent.getElement());
+    }
+  };
+}
