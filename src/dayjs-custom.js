d7c535b0a37ee57b8f13ad919f29696daf919938
Gleb Limanskiy <33000609+UnicalN@users.noreply.github.com>
2022-06-24 18:51:19 +0300

    sort

--- src/dayjs-custom.js
+++ src/dayjs-custom.js
@@ -22,3 +22,47 @@ export const getDurationFromIso = (start, finish) => {
   if ((duration.days < 10)&&(duration.days > 0)){humanizedDuration = `0${humanizedDuration}`;}
   return (humanizedDuration);
 };
+
+const getDuration = (start, finish)=>dayjs(finish).diff(dayjs(start));
+
+
+const getWeightForNull = (a, b) => {
+  if (a === null && b === null) {
+    return 0;
+  }
+
+  if (a === null) {
+    return 1;
+  }
+
+  if (b === null) {
+    return -1;
+  }
+
+  return null;
+};
+
+
+export const sortByDay = (a, b) => {
+  a = a.date_from;
+  b = b.date_from;
+  const weight = getWeightForNull(a, b);
+
+  return weight ?? getDuration(a, b);
+};
+
+export const sortByTime = (a, b) => {
+  a = getDuration(a.date_from, a.date_to);
+  b = getDuration(b.date_from, b.date_to);
+  const weight = getWeightForNull(a, b);
+
+  return weight ?? (b - a);
+};
+
+export const sortByPrice = (a, b) => {
+  a = a.base_price;
+  b = b.base_price;
+  const weight = getWeightForNull(a, b);
+
+  return weight ?? (b - a);
+};
