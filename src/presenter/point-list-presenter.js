8dbbb9270b46a9cfab6a354101496ec4a069fc75
Gleb Limanskiy <33000609+UnicalN@users.noreply.github.com>
2022-06-27 07:05:45 +0300

    points render fix

--- src/presenter/point-list-presenter.js
+++ src/presenter/point-list-presenter.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import PointsModel from '../model/points-model.js';
 import {render,  RenderPosition} from '../framework/render.js';
 import NewPointView from '../view/new-point-view.js';
@@ -5,10 +6,8 @@ import PointListView from '../view/point-list-view.js';
 import SortView from '../view/sort-view.js';
 import NoPointsView from '../view/no-points-view.js';
 
-//import OffersModel from '../model/offers-model.js';
-//import DestinationsModel from '../model/destinations-model.js';
 import PointPresenter from './point-presenter.js';
-//import { updateItem } from '../update-item.js';
+;
 import {SortType, UpdateType, UserAction} from '../const.js';
 import { sortByDay, sortByPrice, sortByTime } from '../dayjs-custom.js';
 
@@ -16,13 +15,9 @@ export default class PointListPresenter {
 
   #pointListComponent = new PointListView();
   #pointListContainer = null;
-  //#pointsList= null;
-  //#offersList= null;
-  //#destinations = null;
 
   #pointsModel = new PointsModel();
-  //#offersModel = new OffersModel();
-  //#destinationsModel = new DestinationsModel();
+
   #pointPresenter = new Map();
   #sortComponent = null;
   constructor(){
@@ -33,25 +28,25 @@ export default class PointListPresenter {
   get points() {
     switch (this.#currentSortType) {
       case SortType.DAY:
-        return [...this.#pointsModel.tasks].sort(sortByDay);
+        return [...this.#pointsModel.points].sort(sortByDay);
       case SortType.TIME:
-        return [...this.#pointsModel.tasks].sort(sortByTime);
+        return [...this.#pointsModel.points].sort(sortByTime);
       case SortType.PRICE:
-        return [...this.#pointsModel.tasks].sort(sortByPrice);
+        return [...this.#pointsModel.points].sort(sortByPrice);
     }
     return this.#pointsModel.points;
   }
 
 
   #handleModeChange = () => {
-    //console.log('handleModeChange');
+
     this.#pointPresenter.forEach((presenter) => presenter.resetView());
   };
 
   #renderPoint = (point) => {
 
     const pointPresenter = new PointPresenter(this.#pointListComponent.element, this.#handleViewAction, this.#handleModeChange);
-    //console.log ('point list presenter 30', point);
+
     pointPresenter.init(point);
     this.#pointPresenter.set(point.id, pointPresenter);
   };
@@ -60,16 +55,20 @@ export default class PointListPresenter {
     this.#pointPresenter.forEach((presenter) => presenter.destroy());
     this.#pointPresenter.clear();
   };
+  //----------------------------------------------
 
   #renderSort = () => {
     this.#sortComponent = new SortView(this.#currentSortType);
     this.#sortComponent.setSortChangeHandler(this.#handleSortChange);
+
     render(this.#sortComponent, this.#pointListComponent.element, RenderPosition.AFTERBEGIN);
   };
 
   #renderPointList = () => {
+    console.log(this.#pointListComponent.element, this.#pointListContainer);
     render(this.#pointListComponent, this.#pointListContainer);
   };
+  //----------------------------------------------------------------------
 
   #renderNewPoint = () => {
     render(new NewPointView(), this.#pointListComponent.element);
@@ -80,47 +79,30 @@ export default class PointListPresenter {
   };
 
   #renderAllPoints = (points) => {
+    if (!points) {
+      this.#renderNoPoints();
+      return;
+    }
     points.forEach((point) => {
-      //console.log ('point list presenter 58',point);
       this.#renderPoint(point);
     });
   };
 
   #currentSortType = SortType.DEFAULT;
-  //#sourcedPointsList = [];
 
   init = (pointListContainer) => {
     this.#pointListContainer = pointListContainer;
-    //this.#pointsList = [...this.#pointsModel.points];
-
-    //this.#offersList = [...this.#offersModel.offers];
-    //this.#destinations = [...this.#destinationsModel.destinations];
 
     this.#renderSort();
 
-    //this.#sourcedPointsList = [...this.#pointsModel.points];
-
     this.#renderPointList();
+    this.#renderAllPoints(this.points);
 
 
-  };
-
-  #renderList = () => {
-    if ([...this.#pointsModel.points].length === 0) {
-      this.#renderNoPoints();
-      return;
-    }
-    //this.#renderNewPoint();
-    console.log(this.#pointListComponent, this.#pointListContainer)
-    render(this.#pointListComponent, this.#pointListContainer);
   };
 
   #handlePointChange = (updatedPoint) => {
-    //console.log('handlePoint begin', updatedPoint);
-    //this.#pointsList = updateItem(this.#pointsList, updatedPoint);
-    //this.sourcedPointsList = updateItem(this.#sourcedPointsList, updatedPoint);
-    //this.#pointPresenter.get(updatedPoint.id).init(updatedPoint);
-    //console.log('handlePoint end', updatedPoint);
+
   };
 
   #handleViewAction = (actionType, updateType, update) => {
@@ -147,43 +129,24 @@ export default class PointListPresenter {
       case UpdateType.MINOR:
         // - обновить список (например, когда задача ушла в архив)
         this.#clearPointsList();
-        this.#renderAllPoints(); //points list?______
+        this.#renderAllPoints(data); //points list?______
         break;
       case UpdateType.MAJOR:
         // - обновить всю доску (например, при переключении фильтра)
         this.#clearPointsList({resetRenderedTaskCount: true, resetSortType: true});
-        this.#renderAllPoints(); //points list?_____
+        this.#renderAllPoints(data); //points list?_____
         break;
     }
   };
 
-  /* #sortPoints = (sortType) => {
-    switch (sortType) {
-      case SortType.DAY:
-        this.#pointsList.sort(sortByDay);
-        break;
-      case SortType.TIME:
-        this.#pointsList.sort(sortByTime);
-        break;
-      case SortType.PRICE:
-        this.#pointsList.sort(sortByPrice);
-        break;
-      default:
-        this.#pointsList = [...this.#sourcedPointsList];
-    }
-
-    this.#currentSortType = sortType;
-
-  }; */
-
   #handleSortChange = (sortType) => {
-    console.log(this.#currentSortType, sortType)
+    console.log(this.#currentSortType, sortType);
     if (this.#currentSortType === sortType) {
       return;
     }
     this.#currentSortType = (sortType);
     this.#clearPointsList({resetRenderedTaskCount: true});
-    this.#renderList();
+    this.#renderAllPoints(this.points);
   };
 
 }
